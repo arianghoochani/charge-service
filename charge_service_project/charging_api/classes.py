dfe51fdc74aa054b663ecf42335275e5a62cc245
arianghoochani <arian_gh22@yahoo.com>
2025-03-15 23:51:51 +0100

    insert request to db

--- charge_service_project/charging_api/classes.py
+++ charge_service_project/charging_api/classes.py
@@ -1,5 +1,33 @@
-class ChargingRequestValidator:
+class ChargingRequestValidatorInput:
     def __init__(self, station_id, driver_token, callback_url):
         self.station_id = station_id
         self.driver_token = driver_token
         self.callback_url = callback_url
+
+class ChargingRequestValidatorResponse:
+    statusDB = {
+        "accepted":{
+            "status": "accepted",
+            "message":"Request is being processed asynchronously. The result will be sent to the provided callback URL."
+        },
+        "station_id":{
+            "status": "rejected",
+            "message":"station_id is invalid."
+        },
+         "driver_token":{
+            "status": "rejected",
+            "message":"driver_token is invalid."
+        },
+        "callback_url":{
+            "status": "rejected",
+            "message":"callback_url is invalid."
+        },
+        "unknown":{
+            "status": "rejected",
+            "message":"unknown error has been occured."
+        }
+    }
+    def __init__(self, status = "accepted"):
+        self.status = ChargingRequestValidatorResponse.statusDB[status]["status"]
+        self.message = ChargingRequestValidatorResponse.statusDB[status]["message"]
+
--- charge_service_project/charging_api/views.py
+++ charge_service_project/charging_api/views.py
@@ -1,13 +1,34 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .serializers import ChargingRequestValidatorSerializer
-@api_view(['GET'])
+from rest_framework.serializers import ValidationError
+from django.utils.timezone import now 
+from .serializers import ChargingRequestValidatorInputSerializer, ChargingRequestValidatorResponseSerializer
+from .classes import ChargingRequestValidatorResponse
+from .models import ChargingRequestLog
+
+@api_view(['POST'])
 def chargingRequestValidator(request):
-    f = {}
+    status = "unknown"
     try:
-        serializer = ChargingRequestValidatorSerializer(data=request.data)
+        serializer = ChargingRequestValidatorInputSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        f = {"message":"ok"}
+        status = "accepted"
+        chargingRequestLog = ChargingRequestLog(
+                station_id=serializer.validated_data["station_id"],
+                driver_token=serializer.validated_data["driver_token"],
+                callback_url=serializer.validated_data["callback_url"],
+                request_time=now(),
+                decision_time=None,
+                decision="unknown"
+            )
+        chargingRequestLog.save(force_insert=True)
+    except ValidationError :
+        status_code = "0"
+        attributeName = list(serializer.errors.keys())[0]
+        status = attributeName
     except:
-        f = {"message":"error"}
-    return Response f
+        status = "unknown"
+
+    chargingRequestValidatorResponse = ChargingRequestValidatorResponse(status = status)
+    serializer = ChargingRequestValidatorResponseSerializer(chargingRequestValidatorResponse)
+    return Response(serializer.data)
