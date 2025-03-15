34d9d92edf0452e5b51c7d374c79c0f9c10c13e4
arianghoochani <arian_gh22@yahoo.com>
2025-03-15 21:48:22 +0100

    verify validate view

--- charge_service_project/charging_api/views.py
+++ charge_service_project/charging_api/views.py
@@ -3,4 +3,11 @@ from rest_framework.decorators import api_view
 from .serializers import ChargingRequestValidatorSerializer
 @api_view(['GET'])
 def chargingRequestValidator(request):
-    return Response("hello")
+    f = {}
+    try:
+        serializer = ChargingRequestValidatorSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        f = {"message":"ok"}
+    except:
+        f = {"message":"error"}
+    return Response f
