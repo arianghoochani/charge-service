baaf125bf28594ec5ae2c0fced0da5ba0d709e44
Arian Ghoochani <68280024+arianghoochani@users.noreply.github.com>
2025-03-16 00:38:53 +0100

    Delete charge_service_project directory

--- charge_service_project/charging_api/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ChargingApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'charging_api'
--- charge_service_project/charging_api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path,include
-from . import views
-
-urlpatterns = [
-    path('chargingRequestValidator/',views.chargingRequestValidator,name='chargingRequestValidator'),
-    path('getrequestlog/',views.getRequestLog,name='getRequestLog'),
-    path('addacl/',views.addACL,name='addACL'),
-    ]
