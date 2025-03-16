39537c2e05431d906ac0d6b4305fb8195d38a486
arianghoochani <arian_gh22@yahoo.com>
2025-03-16 16:40:47 +0100

    try

--- dockerized_server/charge_server/chargedjango/charging_api/views.py
+++ dockerized_server/charge_server/chargedjango/charging_api/views.py
@@ -58,35 +58,35 @@ def checkAuthority(request):
     message = ""
     decisionTime = now()
     checkAuthorityRequest = CheckAuthorityRequest()
-    try:
-        serializer = CheckAuthorityRequestSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        checkAuthorityRequest = serializer.save()
-        request_time = datetime.fromisoformat(checkAuthorityRequest.replace("Z", "+00:00"))
-        decision_time = datetime.fromisoformat(now().replace("Z", "+00:00"))
-        time_difference = decision_time - request_time
-        if time_difference <= timedelta(minutes=2):
-            ACl_id = checkAuthorityRequest.station_id + checkAuthorityRequest.driver_token
-            if AccessControlList.objects.filter(ACL_id=ACL_id).exists():
-                decision = "allowed"
-                message = "Access granted"
-            else:
-                decision = "not_allowed"
-                message = "Access denied"
+    # try:
+    serializer = CheckAuthorityRequestSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    checkAuthorityRequest = serializer.save()
+    request_time = datetime.fromisoformat(checkAuthorityRequest.replace("Z", "+00:00"))
+    decision_time = datetime.fromisoformat(now().replace("Z", "+00:00"))
+    time_difference = decision_time - request_time
+    if time_difference <= timedelta(minutes=2):
+        ACl_id = checkAuthorityRequest.station_id + checkAuthorityRequest.driver_token
+        if AccessControlList.objects.filter(ACL_id=ACL_id).exists():
+            decision = "allowed"
+            message = "Access granted"
         else:
-            decision = "unknown"
-            message = "Request is too old"
-        chargingRequestLog = ChargingRequestLog(
-        station_id=checkAuthorityRequest.station_id,
-        driver_token= checkAuthorityRequest.driver_token,
-        callback_url=checkAuthorityRequest.callback_url,
-        request_time=checkAuthorityRequest.request_time,
-        decision_time=decision_time,
-        decision=decision
-    )
-        chargingRequestLog.save(force_insert=True)
-    except:
-        message = "An error occurred while proccessing charging request."
+            decision = "not_allowed"
+            message = "Access denied"
+    else:
+        decision = "unknown"
+        message = "Request is too old"
+    chargingRequestLog = ChargingRequestLog(
+    station_id=checkAuthorityRequest.station_id,
+    driver_token= checkAuthorityRequest.driver_token,
+    callback_url=checkAuthorityRequest.callback_url,
+    request_time=checkAuthorityRequest.request_time,
+    decision_time=decision_time,
+    decision=decision
+)
+    chargingRequestLog.save(force_insert=True)
+    # except:
+    #     message = "An error occurred while proccessing charging request."
     if checkAuthorityRequest.callback_url:
         callbackresponse = requests.post(checkAuthorityRequest.callback_url, json={"message": message})
     checkAuthorityResponse = CheckAuthorityResponse(message = message)
