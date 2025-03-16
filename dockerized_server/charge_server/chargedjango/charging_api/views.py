ec384641950a001a73c6af4a6b32f132ef094852
arianghoochani <arian_gh22@yahoo.com>
2025-03-16 11:02:34 +0100

    remove try for debug

--- dockerized_server/charge_server/chargedjango/charging_api/views.py
+++ dockerized_server/charge_server/chargedjango/charging_api/views.py
@@ -17,33 +17,32 @@ producer = Producer(producer_conf)
 @api_view(['POST'])
 def chargingRequestValidator(request):
     status = "unknown"
-    try:
-        serializer = ChargingRequestValidatorInputSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        status = "accepted"
-        # chargingRequestLog = ChargingRequestLog(
-        #         station_id=serializer.validated_data["station_id"],
-        #         driver_token=serializer.validated_data["driver_token"],
-        #         callback_url=serializer.validated_data["callback_url"],
-        #         request_time=now(),
-        #         decision_time=None,
-        #         decision="unknown"
-        #     )
-        # chargingRequestLog.save(force_insert=True)
-        message = {
-            "station_id": serializer.validated_data["station_id"],
-            "driver_token": serializer.validated_data["driver_token"],
-            "callback_url": serializer.validated_data["callback_url"],
-            "request_time": now(),
-        }
-        producer.produce(TOPIC_NAME, json.dumps(message))
-        producer.flush()
-    except ValidationError :
-        status_code = "0"
-        attributeName = list(serializer.errors.keys())[0]
-        status = attributeName
-    except:
-        status = "unknown"
+    # try:
+    serializer = ChargingRequestValidatorInputSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    status = "accepted"
+    # chargingRequestLog = ChargingRequestLog(
+    #         station_id=serializer.validated_data["station_id"],
+    #         driver_token=serializer.validated_data["driver_token"],
+    #         callback_url=serializer.validated_data["callback_url"],
+    #         request_time=now(),
+    #         decision_time=None,
+    #         decision="unknown"
+    #     )
+    # chargingRequestLog.save(force_insert=True)
+    message = {
+        "station_id": serializer.validated_data["station_id"],
+        "driver_token": serializer.validated_data["driver_token"],
+        "callback_url": serializer.validated_data["callback_url"],
+        "request_time": now(),
+    }
+    producer.produce(TOPIC_NAME, json.dumps(message))
+    producer.flush()
+    # except ValidationError :
+    #     attributeName = list(serializer.errors.keys())[0]
+    #     status = attributeName
+    # except:
+    #     status = "unknown"
 
     chargingRequestValidatorResponse = ChargingRequestValidatorResponse(status = status)
     serializer = ChargingRequestValidatorResponseSerializer(chargingRequestValidatorResponse)
