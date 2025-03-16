05637c168d2356d94c73742aee9f4eb444b96f9f
arianghoochani <arian_gh22@yahoo.com>
2025-03-16 10:57:33 +0100

    fix validators

--- dockerized_server/charge_server/chargedjango/charging_api/validators.py
+++ dockerized_server/charge_server/chargedjango/charging_api/validators.py
@@ -2,12 +2,13 @@ from django.core.exceptions import ValidationError
 import re
 
 def validate_UUID(value) :
-    if not re.search("^[0-9A-Za-z\-]*$", value) :
-        raise ValidationError(params={"uuid" : value}, message="invalid uuid")
+    if not re.search("^[0-9A-Za-z]*$", value) :
+        raise ValidationError(params={"endCharacter" : value}, message="invalid endCharacter")
 
 def validate_token(value) :
-    if not re.search("^[a-zA-Z0-9\-\_~.]+$", value) :
-        raise ValidationError(params={"driver_token" : value}, message="invalid driver_token")
+    if not re.search("^[0-9A-Za-z\-\_~.]*$", value) :
+        raise ValidationError(params={"endCharacter" : value}, message="invalid endCharacter")
+
 
 def validate_URL(value) :
     if not re.search("^http[s]{0,1}:\/\/[0-9a-zA-Z\?\-_&=:\.\/]{3,250}$", value) :
