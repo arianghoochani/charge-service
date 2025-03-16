19d471eaedfd03308e8697f5921bf9f9c0c4e5c6
arianghoochani <arian_gh22@yahoo.com>
2025-03-16 17:17:24 +0100

    add time

--- dockerized_server/charge_server/chargedjango/charging_api/views.py
+++ dockerized_server/charge_server/chargedjango/charging_api/views.py
@@ -62,15 +62,18 @@ def checkAuthority(request):
     serializer = CheckAuthorityRequestSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
     checkAuthorityRequest = serializer.save()
-    # request_time = datetime.fromisoformat(checkAuthorityRequest.request_time)
+    # request_time = datetime.fromisoformat(str(checkAuthorityRequest.request_time))
     # request_time = datetime.fromisoformat(checkAuthorityRequest.request_time.replace("Z", "+00:00"))
-    print(f"request_time : {checkAuthorityRequest.request_time}")
-    print(f"request_time : {type(checkAuthorityRequest.request_time)}")
-    # decision_time = datetime.fromisoformat(now().replace("Z", "+00:00"))
+    request_time = datetime.fromisoformat(str(checkAuthorityRequest.request_time).replace("Z", "+00:00"))
+    print(f"request_time : {request_time}")
+    print(f"request_time : {type(request_time)}")
+    decision_time = datetime.fromisoformat(str(decision_time).replace("Z", "+00:00"))
     print(f"decision_time : {decision_time}")
     print(f"decision_time : {type(decision_time)}")
     # time_difference = decision_time - request_time
     # if time_difference <= timedelta(minutes=2):
+    time_difference = abs(decision_time - request_time)
+    print(time_difference)
     if 1 == 1:
         ACL_id = checkAuthorityRequest.station_id + checkAuthorityRequest.driver_token
         if AccessControlList.objects.filter(ACL_id=ACL_id).exists():
